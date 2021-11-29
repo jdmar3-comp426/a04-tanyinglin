45db3fab131be29b4310a6c8eeabee2bc66cd95d
Tanying <tlin74832@gmail.com>
2021-11-28 21:02:27 -0500

    test get

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var db = require("./database.js");
 // Require md5 MODULE
 var md5 = require("md5");
 // Make Express use its own built-in body parser
+var bodyParser = require("body-parser");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -13,11 +14,11 @@ app.use(express.json());
 const HTTP_PORT = 5000;
 // Start server
 app.listen(HTTP_PORT, () => {
-    //console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
 });
 // READ (HTTP method GET) at root endpoint /app/
 app.get("/app/", (req, res, next) => {
-    //res.json({"message":"Your API works! (200)"});
+    res.json({"message":"Your API works! (200)"});
 	res.status(200);
 });
 
@@ -26,7 +27,7 @@ app.get("/app/", (req, res, next) => {
 app.post("/app/new/", (req, res) => {
 	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)");
 	const info = stmt.run(req.body.user, md5(req.body.pass));
-	res.status(201).send({"message": info.changes + " record created: ID " + info.lastInsertRowid + " (201)"});
+	res.status(201).json({"message": info.changes + " record created: ID " + info.lastInsertRowid + " (201)"});
 	
 });
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
@@ -36,6 +37,11 @@ app.get("/app/users", (req, res) => {
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
+app.get("/app/user/:id",(req,res) => {
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?");
+	const out = stmt.get(req.params.id);
+	res.status(200).json(out);
+})
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 
